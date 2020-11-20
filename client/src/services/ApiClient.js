3b8a5f52c40496fa254a8af448c56850469b4d1e
Jackie Cheung <jackcheung56@gmail.com>
2020-11-20 15:39:03 -0500

    maybe working

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const ApiClient = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-    ? `${window.location.origin}/api`
-    : 'http://localhost:3001/hp/api'
+    ? `${window.location.origin}/hp`
+    : 'http://localhost:3001/hp/'
     
 })
 
